d98be2f0bf5ff3dd6ac1f5271ad2224c06920110
aichadjalobalde <aichad33@nycstudents.net>
2026-02-09 10:46:36 -0500

    Update businesssolution.py

--- businesssolution.py
+++ businesssolution.py
@@ -9,6 +9,32 @@ from tkinter.ttk import *
 # Create Object
 root = Tk() 
 
+# dev
+def blah():
+	with open("demofile.txt", "w") as f:
+		f.write(T.get("1.0",END))
+
+def blahblah():
+	with open("demofile.txt", "r") as f:
+		T.insert("1.0",f.read())
+
+# #idk
+
+
+
+# print("You entered:")
+# print(text)
+
+# buttons
+write_button = Button(root, text="Write To File", command=blah)
+read_button = Button(root, text="Read From File", command=blahblah)
+
+
+# Place widgets in window (with pack function!)(order matters)
+write_button.pack()
+read_button.pack()
+#Tk.pack()
+
 # Initialize tkinter window with dimensions 100x100             
 
 camera = Button(root, text = 'Camera')
